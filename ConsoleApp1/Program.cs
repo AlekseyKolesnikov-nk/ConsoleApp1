216bed7e9eb2233548ebc0fecf6e04e5f5161772
AlekseyKolesnikov-nk <Aleksey.Kolesnikov@evraz.com>
2024-08-21 22:26:08 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class MainClass {
+    public static void Main(string[] args) {
+        
+        string myName;
+        myName = "Alex";
+
+        Console.WriteLine(myName);
+
+        Console.ReadKey();
+    } 
+}
