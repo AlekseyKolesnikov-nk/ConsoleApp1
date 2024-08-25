ac37738234021148b634167a580cd4fb3c0b044b
AlekseyKolesnikov-nk <Aleksey.Kolesnikov@evraz.com>
2024-08-25 18:21:26 +0700

    Модуль 3.7 Финальная практика модуля

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 
-class MainClass {
-    public static void Main(string[] args) {
-        
-        string myName;
-        myName = "Alex";
+class MainClass
+{
+    public static void Main(string[] args)
+    {
 
-        Console.WriteLine(myName);
-
+        Console.Write("Enter your name: ");
+        var name  = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        var age = checked((byte)int.Parse(Console.ReadLine()));
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        Console.Write("Enter your birthdate: ");
+        var birthdate  = Console.ReadLine();
+        Console.WriteLine("Your birhtdate is {0} ", birthdate);
         Console.ReadKey();
-    } 
+
+    }
 }
