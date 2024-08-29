009c23ae18cbc0494438c478219347bbcf50707e
AlekseyKolesnikov-nk <Aleksey.Kolesnikov@evraz.com>
2024-08-29 21:24:39 +0700

    4.5.6 Опрос трех пользователей

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,44 +1,133 @@
 ﻿using System;
+using System.ComponentModel.Design;
 class MainClass
 {
     public static void Main(string[] args)
     {
-        int[,] mmm = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-
-        // формируем построчный вывод массива без сортировки
-        for (int v = 0; v < mmm.GetUpperBound(0) + 1; v++)
-        {
-            for (int w = 0; w < mmm.GetUpperBound(1) + 1; w++)
-                Console.Write(mmm[v, w] + " ");
-            Console.WriteLine();
-        }
+        int n;
+        Console.WriteLine("Введите количество пользователей");
+        n = Convert.ToInt32(Console.ReadLine());
         
-        Console.WriteLine();
-        int x;
-
-        // формируем построчный вывод массива без сортировки
-        for (int i = 0; i <= mmm.GetUpperBound(0); i++)
+        for (int k = 0; k < n ; k++)
         {
-            for (int j = 0; j <= mmm.GetUpperBound(1); j++)
+            (string Name1, string Name2, string Login, int LoginLenght, bool Pet, double Age, string [] favColors) User;
+
+            Console.WriteLine("Введите имя");
+            User.Name1 = Console.ReadLine();
+
+            Console.WriteLine("Введите Фамилию");
+            User.Name2 = Console.ReadLine();
+
+            Console.WriteLine("Введите логин");
+            User.Login = Console.ReadLine();
+            User.LoginLenght = User.Login.Length;
+
+            Console.WriteLine("Есть ли у вас животные? Да или Нет");
+            var res = Console.ReadLine();
+            if (res == "Да")
             {
-                for (int k = j + 1; k <= mmm.GetUpperBound(1); k++)
-                    if (mmm[i, j] > mmm[i, k])
-                    {
-                        x = mmm[i, k];
-                        mmm[i, k] = mmm[i, j];
-                        mmm[i, j] = x;
+                User.Pet = true;
             }
-        }
+            else
+            {
+                User.Pet = false;
+            }
 
-        // формируем построчный вывод массива с сортировкой
-        for (int v = 0; v < mmm.GetUpperBound(0) + 1; v++)
-        {
-            for (int w = 0; w < mmm.GetUpperBound(1) + 1; w++)
-                Console.Write(mmm[v, w] + " ");
+            Console.WriteLine("Введите возраст пользователя");
+            User.Age = double.Parse(Console.ReadLine());
+
+            User.favColors = new string[3];
+            Console.WriteLine("Введите три любимых цвета пользователя");
+            for (int i = 0; i < User.favColors.Length; i++)
+            {
+                User.favColors[i] = Console.ReadLine();
+            }
             Console.WriteLine();
         }
     }
 }
+
+
+//Работа с кортежем
+
+        //(string Name, string Type, double Age, int NameCount) Pet;
+
+        //Console.WriteLine("Имя питомца:");
+        //Pet.Name = Console.ReadLine();
+        //Console.WriteLine(Pet.Name);
+        //Pet.NameCount = Pet.Name.Length;
+        //Console.WriteLine(Pet.NameCount);
+
+        //Console.WriteLine("Порода питомца:");
+        //Pet.Type = Console.ReadLine();
+        //Console.WriteLine(Pet.Type);
+
+        //Console.WriteLine("Возраст питомца:");
+        //Pet.Age = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine(Pet.Age);
+
+// ---------------------------------------------------------------------------
+        //var (name, age) = ("Алексей", 59);
+        //Console.WriteLine("Мое имя: {0}", name);
+        //Console.WriteLine("Мой возраст: {0}" ,age);
+        
+        //Console.WriteLine("Введите Ваше имя: ");
+        //name = Console.ReadLine();
+
+        //Console.WriteLine("Введите Ваш возраст цифрами: ");
+        //age = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine();
+
+        //Console.WriteLine("Ваше имя: {0}", name);
+        //Console.WriteLine("Ваш возраст: {0}" ,age);
+
+//------------------------------------------------------------------------------------
+
+//var anketa = (name: "Алексей", age: 59, plant: "ЕЗСМК", city: "Новокузнецк");
+
+//Console.WriteLine("Ваше имя: {0}", anketa.name);
+//Console.WriteLine("Ваш возраст: {0}", anketa.age);
+//Console.WriteLine("Ваше предприятие: {0}", anketa.plant);
+//Console.WriteLine("Ваш город: {0}", anketa.city);
+
+
+
+
+
+//int[,] mmm = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+//// формируем построчный вывод массива без сортировки
+//for (int v = 0; v < mmm.GetUpperBound(0) + 1; v++)
+//{
+//    for (int w = 0; w < mmm.GetUpperBound(1) + 1; w++)
+//        Console.Write(mmm[v, w] + " ");
+//    Console.WriteLine();
+//}
+
+//Console.WriteLine();
+//int x;
+
+//// формируем построчный вывод массива без сортировки
+//for (int i = 0; i <= mmm.GetUpperBound(0); i++)
+//{
+//    for (int j = 0; j <= mmm.GetUpperBound(1); j++)
+//    {
+//        for (int k = j + 1; k <= mmm.GetUpperBound(1); k++)
+//            if (mmm[i, j] > mmm[i, k])
+//            {
+//                x = mmm[i, k];
+//                mmm[i, k] = mmm[i, j];
+//                mmm[i, j] = x;
+//    }
+//}
+
+//// формируем построчный вывод массива с сортировкой
+//for (int v = 0; v < mmm.GetUpperBound(0) + 1; v++)
+//{
+//    for (int w = 0; w < mmm.GetUpperBound(1) + 1; w++)
+//        Console.Write(mmm[v, w] + " ");
+//    Console.WriteLine();
+//}
 
 //class MainClass
 //{
